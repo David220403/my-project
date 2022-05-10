96b867547a754f63e2996cb7922ef58656bd86e8
Raihan <17radf@gmail.com>
2022-05-11 00:07:03 +0700

    nambahin multi level login cuy

--- src/com/Koneksi/Koneksi.java
+++ src/com/Koneksi/Koneksi.java
@@ -10,21 +10,20 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
 public class Koneksi {
-   
-        private static Connection mysqlconfig;
-        public static Connection configDB()throws SQLException{
-            try {
-                String url, user, pass;
-                url = "jdbc:mysql://localhost/db_skincare1";
-                user = "root";
-                pass = "";
-                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-                mysqlconfig = DriverManager.getConnection(url, user, pass);
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, "Database tidak terhubung");
-            }
-            return mysqlconfig;
-        }
+
+	private static Connection mysqlconfig;
+
+	public static Connection configDB() throws SQLException {
+		try {
+			String url, user, pass;
+			url = "jdbc:mysql://localhost/db_skincare1";
+			user = "root";
+			pass = "";
+			mysqlconfig = DriverManager.getConnection(url, user, pass);
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Database tidak terhubung");
+		}
+		return mysqlconfig;
+	}
 }
